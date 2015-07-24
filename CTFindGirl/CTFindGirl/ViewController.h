03a895f763ea0bd86dcc6cc8d6c1f3b9e2b717e6
wangchangfei <877368359@qq.com>
2015-07-24 18:37:28 +0800

    创建一个项目

---
+++ CTFindGirl/CTFindGirl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CTFindGirl
+//
+//  Created by 王长飞 on 15/7/24.
+//  Copyright (c) 2015年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CTFindGirl/CTFindGirl/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CTFindGirl
+//
+//  Created by 王长飞 on 15/7/24.
+//  Copyright (c) 2015年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
