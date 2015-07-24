9768bf3776279f891b6cdb85c714af41c94b1081
wangchangfei <877368359@qq.com>
2015-07-24 19:04:35 +0800

    1

--- CTFindGirl/CTFindGirl/ViewController.m
+++ CTFindGirl/CTFindGirl/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property(nonatomic, strong) NSString *name;
 @end
 
 @implementation ViewController
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
